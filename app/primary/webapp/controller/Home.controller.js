cc794e1c76b395248bd76bd3db0822e9f416bf54
Dale Bezolli <deilbezolli@gmail.com>
2023-07-27 13:58:04 +0000

    Display the list items

--- app/primary/webapp/controller/Home.controller.js
+++ app/primary/webapp/controller/Home.controller.js
@@ -1,24 +1,24 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator",
     "sap/m/StandardListItem",
-], function (Controller, StandardListItem) {
+], function (Controller, Filter, FilterOperator, StandardListItem) {
     "use strict";
 
     return Controller.extend("com.bezolli.primary.controller.Home", {
-        onInit: function () {
-                var oModel = this.getOwnerComponent().getModel("kati");
-                var oContext = oModel.bindContext("/TodoList");
-                oContext.requestObject().then(function(oData) {
-                    // Create a new JSON model
-                    var oJsonModel = new sap.ui.model.json.JSONModel();
-                    // Set the data to the JSON model
-                    oJsonModel.setData(oData);
-                    // Set the model to the view
-                    this.getView().setModel(oJsonModel, "zz");
-                }.bind(this)).catch(function(oError){
-                    console.error("Error", oError);
-                });
-            }
+        displayDetails: function(oEvent) {
+            const oDetails = this.getView().byId("details");            
 
-        });
+            // Find the todolist id
+            const todoListId = oEvent.getSource().getBindingContext().getProperty("ID");
+
+            // Bind the todolist elements to a list
+            oDetails.bindAggregation("items", {
+                path: "/Todo",
+                filters: [new Filter("list_ID", FilterOperator.EQ, todoListId)],
+                template: new StandardListItem({ title: "{text} - {list_ID}" })
+            });
+        }
+    });
 });

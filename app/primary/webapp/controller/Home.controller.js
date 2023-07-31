708d58c85aa33705d9741ec73f65150e1f2d6500
Dale Bezolli <deilbezolli@gmail.com>
2023-07-31 11:09:42 +0000

    Create lists and todos

--- app/primary/webapp/controller/Home.controller.js
+++ app/primary/webapp/controller/Home.controller.js
@@ -1,28 +1,32 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel",
     "sap/ui/model/Filter",
     "sap/ui/model/FilterOperator",
     "sap/m/ColumnListItem",
     "sap/m/Text",
     "sap/m/CheckBox",
-    "sap/m/FlexBox",
     "sap/m/SplitAppMode",
-], function (Controller, Filter, FilterOperator, ColumnListItem, Text, CheckBox, FlexBox, SplitAppMode) {
+], function (Controller, JSONModel, Filter, FilterOperator, ColumnListItem, Text, CheckBox, SplitAppMode) {
     "use strict";
 
     return Controller.extend("com.bezolli.primary.controller.Home", {
         onInit: function() {
+            const oModel = new JSONModel({
+                selectedList: -1
+            });
+            this.getView().setModel(oModel, "state");
+            
             this.updateTodoListDetails();
         },
-        displayDetails: function(oEvent) {
+        displayTodoListDetails: function(iTodoListId) {
             const oDetails = this.getView().byId("details");
-            const oTodoList = oEvent.getSource().getBindingContext();
             const oTextObject = new Text({ text: "{text}" });
             oTextObject.addStyleClass("sapUiTinyMargin");
 
             oDetails.bindAggregation("items", {
                 path: "/Todo",
-                filters: new Filter("list_ID", FilterOperator.EQ, oTodoList.getProperty("ID")),
+                filters: new Filter("list_ID", FilterOperator.EQ, iTodoListId),
                 template: new ColumnListItem({ 
                 cells: [
                     new CheckBox({ selected: "{completed}", select: function() { this.updateTodoListDetails() }.bind(this) }),
@@ -34,7 +38,14 @@ sap.ui.define([
 
             const oSplitContainer = this.getView().byId("container");
             oSplitContainer.setMode(SplitAppMode.ShowHideMode);
-            this.getView().byId("detailsTitle").setText(oTodoList.getProperty("name"));
+            this.getOwnerComponent().getModel().bindList("/TodoList").requestContexts().then(function(aContexts) {
+                const aTodoLists = aContexts.map(oContext => oContext.getObject());
+                const oTodoList = aTodoLists.reduce((accumulator, current) => (!accumulator || accumulator.ID !== iTodoListId) ? current : accumulator);
+                
+                this.getView().byId("detailsTitle").setText(oTodoList.name);
+            })
+
+            this.getView().getModel("state").setProperty("/selectedList", iTodoListId);
         },
         updateTodoListDetails: function() {
             this.getOwnerComponent().getModel().bindList("/Todo", null, null).requestContexts().then(function(aContexts) {
@@ -52,11 +63,42 @@ sap.ui.define([
                 const oTileListContent = this.byId("list").getAggregation("content");
                 for(const oTile of oTileListContent) {
                     const todoListId = oTile.getBindingContext().getProperty("ID");
-                    const oCurrentTodoListDetails = mTodoListDetails.get(todoListId);
+                    const oCurrentTodoListDetails = mTodoListDetails.get(todoListId) || { total: 0, complete: 0 };
 
                     oTile.setSubheader(`Total - ${oCurrentTodoListDetails.total}\nComplete - ${oCurrentTodoListDetails.complete}`);
                 }
             }.bind(this));
+        },
+        onCreateList: function() {
+            const oList = this.getView().byId("list");
+            const iPosition = oList.getBinding("content").getContexts().length;
+            oList.getBinding("content").create({
+                "ID": iPosition + 1,
+                "name": "New List"
+            }, null, true);
+
+            oList.getAggregation("content")[iPosition].firePress();
+        },
+        onCreateTodo: function() {
+            const iSelectedListID = this.getView().getModel("state").getProperty("/selectedList");
+            const oModel = this.getView().getModel().bindList("/Todo")
+            oModel.requestContexts().then(function(aContexts) {
+                const aTodos = aContexts.map(oContext => oContext.getObject());
+                
+                oModel.create({
+                    ID:        aTodos.length + 1,
+                    text:      "New Todo",
+                    completed: false,
+                    list_ID:   iSelectedListID
+                });
+
+                this.displayTodoListDetails(iSelectedListID);
+                this.updateTodoListDetails();
+            }.bind(this));
+        },
+        onDisplayTodoListDetails: function(oEvent) {
+            const iTodoListId = oEvent.getSource().getBindingContext().getProperty("ID");
+            this.displayTodoListDetails(iTodoListId);
         }
     });
 });
